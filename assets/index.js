54e0a600a9871d4b6829a1b94e6a71fa2208d41b
Jino Hong <jinohong1993@gmail.com>
2022-06-13 13:39:18 -0300

    comecando os esbocos, algumas dificuldades, porem parece resolvivel

---
+++ assets/index.js
@@ -0,0 +1,105 @@
+// //START ELEMENTS
+// const body = document.body
+// const sctStart = document.createElement('section')
+// sctStart.setAttribute('display' , 'show')
+// sctStart.innerText = 'Hello welcome to my game!'
+// body.append(sctStart)
+
+// const startBtn =document.createElement('button')
+// startBtn.innerText = "CLICK TO START"
+// sctStart.append(startBtn);
+
+
+
+// //GAME ELEMENTS
+// const sctGame = document.createElement('section');
+// const aBtn = document.createElement('button')
+// aBtn.setAttribute('display' , 'show')
+// const bBtn = document.createElement('button')
+// aBtn.setAttribute('display' , 'none')
+// const cBtn = document.createElement('button')
+// aBtn.setAttribute('display' , 'none')
+// const dBtn = document.createElement('button')
+// aBtn.setAttribute('display' , 'none')
+
+// body.append(sctGame,aBtn,bBtn,cBtn,dBtn);
+
+// //GAME OVER ELEMENTS
+// const sctGameOver = document.createElement('section')
+
+
+// const startSection = document.getElementsByClassName("start");
+// const startBtn = document.getElementsByClassName('startBtn');
+
+// console.log(startSection)
+// console.log(startBtn)
+
+// const querytype = document.querySelector('.startBtn')
+
+// querytype.classList.add("")
+// console.log(querytype)
+
+// const gameBoard = document.querySelector(".game-board")
+// gameBoard.classList.add("show")
+
+// const gameBoard2 = document.getElementsByClassName("game-board")
+// gameBoard2.classList.add("show")
+
+
+const openGame = document.getElementsByClassName("openBtn");
+const startBtn = document.getElementsByClassName("startBtn")
+const gameOver = document.getElementsByClassName("try-again-btn")
+
+const aPress = document.getElementsByClassName("aBtn");
+const bPress = document.getElementsByClassName("bBtn");
+const cPress = document.getElementsByClassName("cBtn");
+const dPress = document.getElementsByClassName("dBtn");
+
+class game {
+    constructor(gameBoard) {
+        this.gameBoard = gameBoard
+
+    }
+
+    boardGenerator (){
+        const gameOrder = [];
+        gameOrder = 
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
